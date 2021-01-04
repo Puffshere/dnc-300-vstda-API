60af13652549ceca3f5cbf71200f9d59924ef0dc
Shawn Taylor <shawncurtistaylor@hotmail.com>
2021-01-04 15:41:33 -0800

    gets rid of unused system directives

--- dnc-300-vstda-api/Models/Todos.cs
+++ dnc-300-vstda-api/Models/Todos.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace dnc_300_vstda_api.Models
+﻿namespace dnc_300_vstda_api.Models
 {
     public class Todos
     {
